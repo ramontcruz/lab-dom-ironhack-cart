e401e0827d808684fae1b18fa65e8891daaf72c8
Ramon Torres Cruz <ramontcruz@gmail.com>
2022-10-26 17:39:05 -0300

    Solved Lab

--- js/index.js
+++ js/index.js
@@ -1,23 +1,44 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
+function updateSubtotal(product) {
+  //console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  let quantity = document.querySelector('.quantity input');
+  let subtotal = parseFloat(price.innerHTML)*quantity.valueAsNumber;
+  let uSubtotal = product.querySelector('.subtotal span');
+  uSubtotal.innerHTML = subtotal;
+  return subtotal;
+; 
   //... your code goes here
 }
 
 function calculateAll() {
+  
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
+  console.log("single "+singleProduct);
+  
+  //const preco = document.getElementsByTagName('td');
+    // ITERATION 2
   //... your code goes here
+  const preco = document.getElementsByClassName('subtotal');
+  const produtos = document.getElementsByClassName('product');
+
+  for (let i=0;i<produtos.length;i++){
+    updateSubtotal(produtos[i]);
+  }
+
 
   // ITERATION 3
   //... your code goes here
+  let total = 0;
+  for (let i=0;i<preco.length;i++){
+    total += parseFloat(preco[i].textContent.replace(/[^0-9]/g,''));
+  }
+  let saidaTotal = document.querySelector('#total-value span');
+  saidaTotal.innerHTML = total;
 }
 
 // ITERATION 4
@@ -40,3 +61,5 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+
